d18f5276773f10e899add4a99e25f94471a52d09
zerpollo <93397349+zerpollo@users.noreply.github.com>
2023-02-13 00:42:19 +0200

    Added ECB operations class which comprises encryption and decryption methods. Also created basic windows form window

---
+++ InformationSecurityTask2/ECBoperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationSecurityTask2
+{
+    public class ECBoperations
+    {
+        public static byte[] Encrypt(string filePath, byte[] key)
+        {
+            byte[] data = File.ReadAllBytes(filePath);
+            byte[] encryptedData;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor();
+                encryptedData = encryptor.TransformFinalBlock(data, 0, data.Length);
+            }
+
+            return encryptedData;
+        }
+        public static byte[] Decrypt(string filePath, byte[] key)
+        {
+            byte[] encryptedData = File.ReadAllBytes(filePath);
+            byte[] decryptedData;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+                decryptedData = decryptor.TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+            }
+
+            return decryptedData;
+        }
+    }
+}
